56144859a0248ca4b3fd0645ac45adfbd813885c
Alan <socyware@gmail.com>
2022-10-25 18:12:10 -0300

    Sale listing

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import { Sale } from "../../models/Sale";
+import { Sale } from "../../models/sale";
 import { BASE_URL } from "../../utils/request";
 import NotificationButton from "../NotificationButton";
 import "./styles.css";
@@ -17,6 +17,10 @@ function SalesCard() {
   const [sales, setSales] = useState<Sale[]>([]);
 
   useEffect(() => {
+    const dmin = minDate.toISOString();
+
+    console.log(minDate);
+
     axios.get(`${BASE_URL}/sales`).then((response) => {
       setSales(response.data.content);
     });
@@ -62,11 +66,11 @@ function SalesCard() {
               return (
                 <tr key={sale.id}>
                   <td className="show992">{sale.id}</td>
-                  <td className="show576">{new Date (sale.date).toLocaleDateString()}</td>
+                  <td className="show576">{new Date(sale.date).toLocaleDateString()}</td>
                   <td>{sale.sellerName}</td>
                   <td className="show992">{sale.visited}</td>
                   <td className="show992">{sale.deals}</td>
-                  <td>{sale.amount}</td>
+                  <td>R$ {sale.amount.toFixed(2)}</td>
                   <td>
                     <div className="dsmeta-red-btn-container">
                       <NotificationButton />
